2202891acea2e7f0ec8844866505f72c0e0c614e
xygenibot <xygeni_scan@xygeni.io>
2025-11-14 14:14:39 +0000

    Fix java.database_access_control issues in src/main/java/org/owasp/webgoat/container/users/UserService.java

--- src/main/java/org/owasp/webgoat/container/users/UserService.java
+++ src/main/java/org/owasp/webgoat/container/users/UserService.java
@@ -50,8 +50,9 @@ public class UserService implements UserDetailsService {
   }
 
   private void createLessonsForUser(WebGoatUser webGoatUser) {
-    jdbcTemplate.execute("CREATE SCHEMA \"" + webGoatUser.getUsername() + "\" authorization dba");
-    flywayLessons.apply(webGoatUser.getUsername()).migrate();
+    String schemaName = webGoatUser.getUsername().replaceAll("[^a-zA-Z0-9]", "");
+    jdbcTemplate.execute("CREATE SCHEMA \"" + schemaName + "\" authorization dba");
+    flywayLessons.apply(schemaName).migrate();
   }
 
   public List<WebGoatUser> getAllUsers() {
