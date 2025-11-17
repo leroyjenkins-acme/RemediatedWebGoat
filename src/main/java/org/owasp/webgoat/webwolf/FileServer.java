c26e2350523f2a130074896485cb44964898bcb2
xygenibot <xygeni_scan@xygeni.io>
2025-11-17 08:42:28 +0000

    Fix 4 issues in src/main/java/org/owasp/webgoat/webwolf/FileServer.java

--- src/main/java/org/owasp/webgoat/webwolf/FileServer.java
+++ src/main/java/org/owasp/webgoat/webwolf/FileServer.java
@@ -14,6 +14,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.attribute.FileTime;
 import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -73,7 +74,8 @@ public class FileServer {
     // DO NOT use multipartFile.transferTo(), see
     // https://stackoverflow.com/questions/60336929/java-nio-file-nosuchfileexception-when-file-transferto-is-called
     try (InputStream is = multipartFile.getInputStream()) {
-      var destinationFile = destinationDir.toPath().resolve(Paths.get(multipartFile.getOriginalFilename()).normalize());
+      String originalFilename = Paths.get(multipartFile.getOriginalFilename()).getFileName().toString();
+      var destinationFile = destinationDir.toPath().resolve(originalFilename).normalize();
       Files.deleteIfExists(destinationFile);
       Files.copy(is, destinationFile);
     }
